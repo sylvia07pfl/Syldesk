919ee08188409e60465aef54f58e0fb559f2fa95
Sylvia Debnath <silvia07002@gmail.com>
2026-08-14 13:57:27 +0530

    Fix Render port binding

--- run.py
+++ run.py
@@ -7,4 +7,8 @@ app = create_app(os.environ.get("FLASK_CONFIG", "default"))
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(
+        host='0.0.0.0',
+        port=int(os.environ.get('PORT', 5000)),
+        debug=False
+    )
